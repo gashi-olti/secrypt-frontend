8e00e2fbb76e464beccfc39da0599b24fa7da741
Oltion Gashi <oltiongashi21@gmail.com>
2024-11-03 15:34:51 +0100

    add twin macro

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { Toaster } from "@/components/ui/toaster";
+
+import GlobalStyles from "@/styles/GlobalStyles";
+import StyledComponentsRegistry from "@/lib/registry";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <StyledComponentsRegistry>
+      <GlobalStyles />
+      <Component {...pageProps} />
+      <Toaster />
+    </StyledComponentsRegistry>
+  );
 }
