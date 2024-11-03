37e207c12dad265493ab7436e2fa6ab36d29c877
Oltion Gashi <oltiongashi21@gmail.com>
2024-11-03 15:31:18 +0100

    create header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import tw from "twin.macro";
+
+export default function Header() {
+  return (
+    <div tw="m-4 md:m-8">
+      <h2 tw="text-3xl">Logo</h2>
+    </div>
+  );
+}
