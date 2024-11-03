b24824e1d83c3740cc2072867626e6fcfbaf120b
Oltion Gashi <oltiongashi21@gmail.com>
2024-11-03 15:31:06 +0100

    create footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import tw from "twin.macro";
+
+export default function Footer() {
+  return (
+    <div tw="m-4 md:m-8">
+      <div tw="w-full flex flex-row justify-between">
+        <p>File Sharing</p>
+        <a href="">Source</a>
+      </div>
+    </div>
+  );
+}
