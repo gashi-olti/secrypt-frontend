e650bdd59cc18f0fda9e35ad0c8d614df7e5487e
Oltion Gashi <oltiongashi21@gmail.com>
2024-11-10 22:13:45 +0100

    add new icons

---
+++ src/components/Icons/Images.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+
+export const Images = {
+  DocIcon: require("../../../public/images/doc_icon.png").default.src,
+  PdfIcon: require("../../../public/images/pdf_icon.svg").default.src,
+  ImageIcon: require("../../../public/images/image_icon.svg").default.src,
+  VideoIcon: require("../../../public/images/video_icon.png").default.src,
+};
