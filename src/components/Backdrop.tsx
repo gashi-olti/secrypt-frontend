9b2e6dd04ad4d38a4800b16c75836a8fb7f3faf9
Oltion Gashi <oltiongashi21@gmail.com>
2024-11-14 22:51:23 +0100

    replace modal backdrop with custom portal backdrop

--- src/components/Backdrop.tsx
+++ src/components/Backdrop.tsx
@@ -1,12 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import tw from "twin.macro";
 import React from "react";
-
-import {
-  AlertDialog,
-  AlertDialogContent,
-  AlertDialogTitle,
-} from "./ui/alert-dialog";
+import ReactDOM from "react-dom";
 
 type Props = {
   isLoading?: boolean;
@@ -15,19 +10,28 @@ type Props = {
 };
 
 export default function Backdrop({ isLoading, description, children }: Props) {
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
     <>
-      <AlertDialog open={isLoading}>
-        <AlertDialogTitle hidden></AlertDialogTitle>
-        <AlertDialogContent tw="h-full bg-transparent border-none outline-none">
-          <div tw="w-full h-full flex flex-col justify-center items-center space-y-4">
-            <div tw="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-gray-100"></div>
-            <h3 tw="text-white">{description ?? "Loading..."}</h3>
-          </div>
-        </AlertDialogContent>
-      </AlertDialog>
-
       {children}
+      {isLoading &&
+        ReactDOM.createPortal(
+          <div
+            tw="fixed inset-0 bg-black/80 flex flex-col justify-center items-center gap-6 backdrop-blur-xs"
+            css={[{ zIndex: 999 }]}
+          >
+            <div tw="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-gray-100"></div>
+            <h3 tw="text-white text-lg">{description ?? "Loading..."}</h3>
+          </div>,
+          document.getElementById("backdrop-root") as HTMLElement
+        )}
     </>
   );
 }
